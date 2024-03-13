5e24b7ece56e202c527981fdf48f28f08bb29a2b
adubasava <anzhalikad@gmail.com>
2024-03-13 11:14:27 +0300

    Create initial structure and initial board

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿const gameBoard = (function() {
+    const board = [];
+
+    for (let i = 0; i < 3; i++) {
+        board[i] = [];
+        for (let j = 0; j < 3; j++) {
+            board[i].push(0);
+        }        
+    }
+
+    return { board };
+})();
